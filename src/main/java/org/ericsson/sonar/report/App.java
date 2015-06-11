9c386fc606ba27c45a5befac29f8e81847c55bc9
deepan <umail2deep@gmail.com>
2015-06-11 19:27:18 +0530

    initial commit

--- src/main/java/org/ericsson/sonar/report/App.java
+++ src/main/java/org/ericsson/sonar/report/App.java
@@ -1,13 +1,32 @@
 package org.ericsson.sonar.report;
 
+import org.apache.log4j.BasicConfigurator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( " Hello  World test once again ...another test removed ..checking merge..." );
-    }
+
+public class App {
+	
+	@Autowired
+	private SonarReportService service;
+
+	public App() {
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"sonar-report.xml");
+		AutowireCapableBeanFactory factory = context
+				.getAutowireCapableBeanFactory();
+		factory.autowireBean(this);
+	}
+
+	public static void main(String[] args) {
+		BasicConfigurator.configure();
+		App app = new App();
+		app.service.generate();
+	}
 }
